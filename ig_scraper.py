35f99b8f7820ba311d217f1a73a5b00cb15b73ef
Kateřina Remešová <remk01@vse.cz>
2023-04-11 15:58:04 +0200

    list of usernames

--- ig_scraper.py
+++ ig_scraper.py
@@ -1,13 +1,11 @@
 import os
 import time
 from instagramy import InstagramUser
-from instagramy.plugins import save_data
 
 def get_user_posts(username):
     user = InstagramUser(username)
     posts = user.posts
     descriptions = []
-
     for post in posts:
         description = post.caption
         descriptions.append(description)
@@ -15,15 +13,21 @@ def get_user_posts(username):
     return descriptions
 
 def main():
-    username = input("Enter the Instagram username: ")
+    #username = input("Enter the Instagram username: ")
+    username = ['breclavsoftball', 'painbusters_most',
+                'spectrumpraha', 'joudrsofficial',
+                'beaverschomutov', 'zraloci_ledenice',
+                'hippos_softball', 'snails_kunovice',
+                'tempo_praha']
     descriptions = get_user_posts(username)
 
     print("\nUser post descriptions:")
     for idx, description in enumerate(descriptions):
         print(f"{idx+1}. {description}")
 
-    # Save data to a file if needed
+    # Save data to a csv file
     save_data(descriptions, f"{username}_post_descriptions.txt", mode="w")
+    df.to_csv('softball_tymy_posts.csv')
 
 if __name__ == "__main__":
     main()
